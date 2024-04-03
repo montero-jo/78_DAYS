db6a66fbc8a02ff4697060e1b76ba1a5459af3eb
Jose Montero <monteroj@ocadu.ca>
2024-04-03 18:38:02 -0400

    v1

---
+++ 78_Days/main.js
@@ -0,0 +1,14 @@
+let sidebar = document.querySelector(".sidebar");
+
+let top = localStorage.getItem("sidebar-scroll");
+if (top !== null) {
+  sidebar.scrollTop = parseInt(top, 10);
+}
+
+window.addEventListener("beforeunload", () => {
+  localStorage.setItem("sidebar-scroll", sidebar.scrollTop);
+});
+
+if('scrollRestoration' in history){
+    history.scrollRestoration='auto'
+    }
